6c37320a2e6c45e44232f66eafea457c1a04cabd
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 03:37:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,5 +60,10 @@ def reduce_to_all_true(source_array
 end
 
 def reduce_to_any_true(source_array)
-  
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
