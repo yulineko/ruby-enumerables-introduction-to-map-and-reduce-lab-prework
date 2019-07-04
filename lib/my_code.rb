45d35102ac0e5e201bb8734c8240ec7a27951586
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 03:38:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,7 +50,7 @@ def reduce_to_total(source_array, starting_point=0)
   return new
 end
 
-ef reduce_to_all_true(source_array)
+def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
     return false if !source_array[i]
