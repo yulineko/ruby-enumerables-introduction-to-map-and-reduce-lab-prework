063095107c2853885d670223c2c1f46717cc70ae
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 03:39:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,7 +30,7 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-  new = [
+  new = []
   i = 0
   while i < source_array.length do
     new.push( source_array[i] * source_array[i] ) # <== Unique work
@@ -40,6 +40,31 @@ def map_to_square(source_array)
 end
 
 
-def reduce_to_total(source_array, starting_point)
-  
+def reduce_to_total(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
